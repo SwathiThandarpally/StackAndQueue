44bbe8c42e07a0ce07d621a9f51aff21fca9b192
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-17 23:48:07 +0530

    Added Stack Operation Program

--- StackAndQueue/Program.cs
+++ StackAndQueue/Program.cs
@@ -5,6 +5,8 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Displaying Stack And Queue Program");
+            Stack stack = new Stack();
+            stack.Display();
         }
     }
 }
---
+++ StackAndQueue/Stack.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public  class Stack
+    {
+        public void Display()
+        {
+
+            Stack<int> stack = new Stack<int>();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            foreach (int item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("Display peek value");
+
+            stack.Peek();
+            Console.WriteLine("The Peek element is " + stack.Peek());
+
+        }
+    }
+}
